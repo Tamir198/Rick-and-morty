a716357a5f45552c6c83701eff21a8932bfd753c
Tamir <1tamir198@gmail.com>
2022-03-26 16:30:56 +0300

    Adding paginate component to the gallery page

--- client/src/pages/charactersGalery/CharacterGallery.js
+++ client/src/pages/charactersGalery/CharacterGallery.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactPaginate from "https://cdn.skypack.dev/react-paginate@7.1.3";
 import AllCharacters from '../../componenets/AllCharacters'
 
+import Styles from './ChracterGalery.module.css'
+
 const CharacterGallery = () => {
+  const [currPage, setCurrPage] = useState('1');
+  const [pageCount, setPageCount] = useState(42);
+
+  const handlePageClick = (event) => {
+    setCurrPage(event.selected)
+  };
+
   return (
     <div>
-      <AllCharacters pageNum="1" />
-      
+      <AllCharacters pageNum={currPage} />
+      <ReactPaginate className={Styles.test}
+         nextLabel="Next"
+         previousLabel="Previous"
+         onPageChange={handlePageClick}
+         pageRangeDisplayed={5}
+         marginPagesDisplayed={3}
+         pageCount={pageCount}
+         breakClassName	={Styles.test}
+         breakLabel="..."
+      />
     </div>
   )
 }
