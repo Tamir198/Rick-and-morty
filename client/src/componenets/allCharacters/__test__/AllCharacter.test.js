b0df4a4db2934ae67b89d860ddea48f78439db2e
Tamir <1tamir198@gmail.com>
2022-05-03 18:57:44 +0300

    Adding test for the AllCharacters components

--- client/src/componenets/allCharacters/__test__/AllCharacter.test.js
+++ client/src/componenets/allCharacters/__test__/AllCharacter.test.js
@@ -2,9 +2,26 @@ import { render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom'
 import AllCharacters from '../AllCharacters'
 
-it('Should render characters grid to the screen',async () => {
-  render(<AllCharacters pageNum={1} />);
-  //TODO fix this not working
-  // const characters =  screen.getByTestId('character');
-  expect(characters).toBeInTheDocument();
-})
+
+describe('Test characters grid', ()=>{
+
+  it('Should render 20 characters on the screen',async () => {
+    render(<AllCharacters pageNum={1} />);
+    const characters = await screen.findAllByTestId('character');
+    expect(characters.length).toBe(20);
+  });
+
+
+  it('Should render the last element from a 20 characters grid ',async () => {
+    render(<AllCharacters pageNum={1} />);
+    const characters = await screen.findAllByTestId('character');
+    expect(characters[characters.length-1]).toBeInTheDocument();
+  });
+
+  it('Should render the first element from a 20 characters grid ',async () => {
+    render(<AllCharacters pageNum={1} />);
+    const characters = await screen.findAllByTestId('character');
+    expect(characters[0]).toBeInTheDocument();
+  });
+
+});
