e3c67ef37e9e167aa253e75a7df45053087b95e4
Tamir <1tamir198@gmail.com>
2022-05-25 14:44:33 +0300

    Adding custom hook for fetching characters data

---
+++ client/src/hooks/useHandleCharactersData.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { addCharactersPage } from '../redux/reducers/charactersSlice'
+
+import useAxios from './useAxios';
+
+/** 
+    * Returns the current characters page data 
+    * 
+    * This hook will check redux store for already existing data, if not 
+    * found - send GET request to the server and update the store.
+    * 
+    @pageNum is the wanted page number (can be found inside AllCharacters component)
+*/
+const useHandleCharactersData = (pageNum) => {
+
+  const { characters } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const [charctersData, setCharctersData] = useState([]);
+  const { fetchData } = useAxios();
+
+  useEffect(() => {
+    const data = characters.find(character => character.pageNum === pageNum);
+
+    if (data) {
+      setCharctersData(data["charctersData"]);
+    } else {
+      fetchData(`allCharacters/${pageNum}`).then(res => {
+        setCharctersData(res.data.characters);
+      });
+      dispatch(addCharactersPage({ pageNum: pageNum - 1, charctersData }));
+    }
+
+  }, [pageNum]);
+
+  return charctersData;
+}
+
+export default useHandleCharactersData;
