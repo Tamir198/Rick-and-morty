64a75a62143af47b58953fb40ab363bd9809aaf1
Tamir <1tamir198@gmail.com>
2022-08-07 17:54:08 +0300

    Adding get and wait method for axios service

--- server/services/axiosService.js
+++ server/services/axiosService.js
@@ -1,12 +1,24 @@
 import axios from 'axios'
 
 axios.defaults.baseURL = 'https://rickandmortyapi.com/';
+
 const axiosService = {
-     fetchData : async (url) => {
-        const res = await axios.get(url);
-        if (res.status !== 200) return 'Error';
-        return res;
-      }
+  fetchData: async (url) => {
+    const res = await axios.get(url);
+    if (res.status !== 200) return 'Error';
+    return res;
+  },
+  /** 
+   * Returns data from multyple requests
+   @urls - array of urls to send GET reqest to
+   */
+  getAndWait: async (urls,cb) => {
+    const res = await axios.all(
+      urls.map(url => axios.get(url))
+      );
+
+    return cb(res);
+  }
 }
 
-export default axiosService;
+export { axiosService };
