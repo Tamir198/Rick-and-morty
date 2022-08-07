23dc757b970cbd6cb13f936a9567233a6d896dfc
Tamir <1tamir198@gmail.com>
2022-08-07 17:53:21 +0300

    Adding general info model

---
+++ server/routes/generalInfo.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+const router = Router();
+
+import {getGeneralInfo} from '../controllers/generalInfoController.js';
+
+router.get("/generalInfo", getGeneralInfo);
+
+export default router;
