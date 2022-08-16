c31a15c2b61fabe3de09ee4031bffa37097c9355
Tamir <1tamir198@gmail.com>
2022-08-16 16:51:23 +0300

    Change functions format

--- client/src/mocks/handlers.js
+++ client/src/mocks/handlers.js
@@ -2,12 +2,11 @@ import { rest } from 'msw';
 import { getSingleCharacter } from './controllers/singleCharater';
 import { getAllCharaters } from './controllers/allCharaters';
 
+//TODO change this to constant confige files 
 const baseAdress = 'http://localhost:4000';
 
+//TODO make this more generic - one handlers array per feature
 export const handlers = [
-  rest.get(`${baseAdress}/singlecharacter/1`
-    , (req, res, ctx) => getSingleCharacter(req, res, ctx)),
-
-  rest.get(`${baseAdress}/allCharacters/1`
-    , (req, res, ctx) => getAllCharaters(req, res, ctx))
+  rest.get(`${baseAdress}/singlecharacter/1`, getSingleCharacter),
+  rest.get(`${baseAdress}/allCharacters/1`, getAllCharaters)
 ]
