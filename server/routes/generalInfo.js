d1c577238cb76bbb5994e4552fdf5046bc4c5b33
Tamir <1tamir198@gmail.com>
2022-09-11 17:08:26 +0300

    Small indentation

--- server/routes/generalInfo.js
+++ server/routes/generalInfo.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 const router = Router();
 
-import {getGeneralInfo} from '../controllers/generalInfoController.js';
+import { getGeneralInfo } from '../controllers/generalInfoController.js';
 
 router.get("/generalInfo", getGeneralInfo);
 
