2dc6f5cc6c174ac7496fc434552af8cc25c1ab1e
Tamir <1tamir198@gmail.com>
2022-09-12 15:56:13 +0300

    Adding password check to login

--- server/services/user.js
+++ server/services/user.js
@@ -1,6 +1,8 @@
 import { UserModel } from "../models/db/user.js";
 
 export default class userService {
-  static getUser = async ( username ) => await UserModel.findOne({ "Name": username });
-  static createuser = ({userName, password}) => console.log('Create new user on mongo')
+  static getUser = async (username, password) => await UserModel.findOne(
+    { "name": username, "password": password }
+  );
+  static createuser = ({ userName, password }) => console.log('Create new user on mongo')
 }
