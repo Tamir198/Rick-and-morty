0f3cd02a54914a527d1d5f6a97d443f5379bc2f4
Tamir <1tamir198@gmail.com>
2022-09-13 15:47:07 +0300

    Remove comment

--- server/services/db/db.js
+++ server/services/db/db.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 
 export function formConnection() {
   try {
-    console.log(process.env.MONGO_URL)
     mongoose.connect(
       process.env.MONGO_URL,
       { useNewUrlParser: true, useUnifiedTopology: true }
