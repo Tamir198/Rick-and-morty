8aeb6b3a138e13a853ce71c7aa8ba74459bd8717
Tamir Abutbul <1tamir198@gmail.com>
2022-10-19 12:26:48 +0300

    change websites list that can send request

--- server/app.js
+++ server/app.js
@@ -10,25 +10,18 @@ import usersRoutes from "./routes/users.js";
 const loadExtensions = async function (app) {
   app.use(cors('*'));
   app.use(express.json());
-
-  app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://rickandmortydocs.netlify.app/');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    // Pass to next layer of middleware
-    next();
-});
-
   formConnection();
 }
 
 const loadRoutes = function (app) {
+
+  app.use((req, res, next) =>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
+  });
+
   app.use(characterRoutes);
   app.use(generalInfoRoutes);
   app.use(usersRoutes);
