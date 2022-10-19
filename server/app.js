10978616d74054dbc31c6e83e0ae219d28c8f883
Tamir <1tamir198@gmail.com>
2022-10-19 11:44:39 +0300

    Adding function to allow cors

--- server/app.js
+++ server/app.js
@@ -11,6 +11,20 @@ const loadExtensions = async function (app) {
   app.use(cors('*'));
   app.use(express.json());
 
+  app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://rickandmortydocs.netlify.app/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Pass to next layer of middleware
+    next();
+});
+
   formConnection();
 }
 
