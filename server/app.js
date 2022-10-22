eb14530e88b0b88fb7c19d9ac4fb5765efb02a65
Tamir Abutbul <1tamir198@gmail.com>
2022-10-22 16:29:12 +0300

    Add Swagger docs

--- server/app.js
+++ server/app.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
+
 import 'dotenv/config'
 import { formConnection } from './services/db/db.js';
 
@@ -11,6 +14,27 @@ const loadExtensions = async function (app) {
   app.use(cors('*'));
   app.use(express.json());
   formConnection();
+  
+  const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Rick and morty",
+        version: "1.0.0",
+        description: "A simple rick and morty thing"
+      },
+      servers: [
+        {
+          url: "http://localhost:4000/"
+        }
+      ]
+    },
+    apis: ['./routes/*.js'],
+  };
+
+  const specs = swaggerJSDoc(options);
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
 }
 
 const loadRoutes = function (app) {
--- server/routes/generalInfo.js
+++ server/routes/generalInfo.js
@@ -2,7 +2,48 @@ import { Router } from 'express';
 const router = Router();
 
 import { getGeneralInfo } from '../controllers/generalInfoController.js';
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     General info:
+ *       type: object
+ *       properties:
+ *         ricksInfo:
+ *           type: string
+ *           description: How many ricks are alive/dead
+ *         mortysInfo:
+ *           type: string
+ *           description: How mnay mortys are alive/dead
+ *       example:
+ *         ricksInfo: "Alive: 29 Dead: 34"
+ *         mortysInfo: "Alive: 54 Dead: 18"
+ */
 
+
+ /**
+  * @swagger
+  * tags:
+  *   name: General Info
+  *   description: General inforamtion regarding how many ricks and mortys are alive
+  */
+
+/**
+ * @swagger
+ * /generalInfo:
+ *   get:
+ *     summary: Returns the data regarding single character
+ *     tags: [General Info]
+ *     responses:
+ *       200:
+ *         description: General information regarding ricks and mortys
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/General info'
+ */
 router.get("/generalInfo", getGeneralInfo);
 
 export default router;
